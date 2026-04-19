a86ccdf3026e5978cd2e42c0ae7853e1f8c60376
Efftsh <rising@gmail.com>
2026-04-19 13:46:10 +0800

    add feature where punctuation and number are blocked from the input by using dom event input to wait for an input event to happen

--- project.js
+++ project.js
@@ -1,16 +1,24 @@
-'use strict';
+"use strict";
 
-const food = ['Burger', 'Pizza', 'Ramen', 'Nasi Goreng', 'Pasta'];
+const food = ["Burger", "Pizza", "Ramen", "Nasi Goreng", "Pasta"];
 
-const button = document.querySelector('.button');
-const buttonList = document.querySelector('.button-list');
-const display = document.querySelector('.display');
-const buttonRandom = document.querySelector('.button-random');
-const input = document.querySelector('.food-input');
-const list = document.querySelector('.list');
-const h1 = document.querySelector('.h1');
+const button = document.querySelector(".button");
+const buttonList = document.querySelector(".button-list");
+const display = document.querySelector(".display");
+const buttonRandom = document.querySelector(".button-random");
+const input = document.querySelector(".food-input");
+const list = document.querySelector(".list");
+const h1 = document.querySelector(".h1");
 
-button.addEventListener('click', function () {
+function blockNumberAndPunctuation(str) {
+  const value = str.replace(/[\p{N}\p{P}]/gu, "");
+  return value;
+}
+input.addEventListener("input", function () {
+  input.value = blockNumberAndPunctuation(input.value);
+});
+
+button.addEventListener("click", function () {
   const newFood = input.value;
   // if (newFood === '') {
   //   alert('Please Enter Food Name!');
@@ -19,41 +27,44 @@ button.addEventListener('click', function () {
   //Short Circuiting
   // newFood || alert('Please Enter Food Name!');
   //Best approach
-  if (newFood === '') {
-    return alert('Please Enter Food Name!');
+  if (newFood === "") {
+    return alert("Please Enter Food Name!");
   }
+
   newFood &&
     food.push(newFood) &&
     alert(`${newFood} has been added to the list`);
 
-  !list.classList.contains('hidden') && list.classList.add('hidden');
-  h1.classList.contains('hidden') && h1.classList.remove('hidden');
+  !list.classList.contains("hidden") && list.classList.add("hidden");
+  h1.classList.contains("hidden") && h1.classList.remove("hidden");
 
-  input.value = '';
+  input.value = "";
 });
 
 //Log All food individually
-buttonList.addEventListener('click', function () {
-  list.textContent = ' Food List';
+buttonList.addEventListener("click", function () {
+  list.textContent = " Food List";
   for (const [no, item] of food.entries()) {
-    const li = document.createElement('li');
+    const li = document.createElement("li");
     li.textContent = `${no + 1}.   ${item}`;
     list.appendChild(li);
   }
-  const clear = document.createElement('button');
-  clear.textContent = 'Clear';
+  const clear = document.createElement("button");
+  clear.textContent = "Clear";
   list.appendChild(clear);
-  clear.addEventListener('click', function () {
-    h1.classList.remove('hidden');
-    list.textContent = '';
+  clear.addEventListener("click", function () {
+    h1.classList.remove("hidden");
+    list.textContent = "";
   });
-  h1.classList.add('hidden');
-  list.classList.remove('hidden');
+  h1.classList.add("hidden");
+  list.classList.remove("hidden");
 });
 
 //Pick a random food
-buttonRandom.addEventListener('click', function () {
+buttonRandom.addEventListener("click", function () {
   const randomFood = Math.trunc(Math.random() * food.length);
   console.log(`Today's food suggestion: ${food[randomFood]}`);
   display.textContent = `Today's food suggestion: ${food[randomFood]}`;
 });
+
+console.log(blockNumberAndPunctuation("123ajdaada:}{{"));
